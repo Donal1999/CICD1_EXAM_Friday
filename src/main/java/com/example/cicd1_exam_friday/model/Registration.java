8f8100b0d80193128f88017a425821b0911f9f38
DONALOMAOILCHIARAIN- <g00365724@atu.ie>
2025-11-21 09:19:38 +0000

    added Registration class and get method

---
+++ src/main/java/com/example/cicd1_exam_friday/model/Registration.java
@@ -0,0 +1,19 @@
+package com.example.cicd1_exam_friday.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+
+@Data
+public class Registration {
+    @NotBlank
+    private String attendeName;
+    @NotBlank @Email
+    private String email;
+    @Pattern(regexp = "TK-[0-9]{4}")
+    private String ticketCode;
+    @Positive
+    private int quantity;
+}
